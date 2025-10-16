61efafbd1e235dda6b0eba7c292290554f5d40fa
Andrew Brown <andrewbrownconsult@gmail.com>
2025-10-16 05:42:13 +0100

    working version before circle title groups

--- frontend/app/components/SimilarGraph.js
+++ frontend/app/components/SimilarGraph.js
@@ -1,18 +1,23 @@
 import * as d3 from 'd3'
 import { useEffect, useRef, useState } from 'react'
 import { fetchMovieDetails, fetchSimilarMovies, getTMDBImagePath } from '../fetchFuncs';
+import { resolve } from 'styled-jsx/css';
 const width = 1000;
 const height = 500;
-
-export default function SimilarGraph({ movieId }) {
-    let numMovies = 0;
+const timeBetweenSpawns = 500;
+export default function SimilarGraph({ movieId, setSelectedMovie }) {
+    const intialized = useRef(false);
+    const cancelLoopRef = useRef(false);
+    const svgRef = useRef(null);
     const [movieData, setMovieData] = useState([]);
-    const linkRef = useRef([]);
+    const [links, setLinks] = useState([]);
 
     async function updateSimilarMovies(mainId) {
         const similarData = await fetchSimilarMovies(mainId);
         console.log(similarData);
         for (const item of similarData.movies) {
+            if (cancelLoopRef.current)
+                break;
             const newDetail = await fetchMovieDetails(item.id);
             const radius = 40;
             const id = newDetail.id;
@@ -20,68 +25,132 @@ export default function SimilarGraph({ movieId }) {
             const x = width * (Math.random())
             const y = height * (Math.random())
             const poster = getTMDBImagePath(newDetail.poster_path, radius);
-            setMovieData(prev => [...prev, { id: id, rad: radius, image: poster, x: x, y: y }]);
-            // linkRef.current.push({ source: movieId, target: item.id });
-            numMovies++;
+            const title = newDetail.title;
+            setMovieData(prev => [...prev, { id: id, rad: radius, image: poster, x: x, y: y, title: title, opacity: 0 }]);
+            setLinks(prev => [...prev, { source: movieId, target: item.id }]);
             await new Promise(resolve =>
-                setTimeout(resolve, 1000));
-            console.log('sleeping for 1 second before next request')
+                setTimeout(resolve, timeBetweenSpawns));
         }
     }
 
-    useEffect(() => {
+    async function getNewMovieData() {
         console.log('movie id is', movieId)
+        cancelLoopRef.current = true;
+        await new Promise(resolve => setTimeout(resolve, timeBetweenSpawns));
+        cancelLoopRef.current = false;
+        d3.select(svgRef.current).select('g').remove();
+        d3.select(svgRef.current).append('g').append('defs')
+        setMovieData([]);
         fetchMovieDetails(movieId).then(res => {
             const radius = 100;
             const id = res.id;
+            const title = res.title
             const poster = getTMDBImagePath(res.poster_path, radius);
-            setMovieData([{ id: movieId, rad: radius, image: poster, x: width / 2, y: height / 2 }]);
+            setMovieData([{ id: movieId, rad: radius, image: poster, x: width / 2, y: height / 2, title: title }]);
         });
         updateSimilarMovies(movieId);
+    }
+    useEffect(() => {
+        getNewMovieData();
     }, [movieId]);
 
+
     useEffect(() => {
-        const selection = d3.select('svg')
-            .attr('width', width)
-            .attr('height', height).append('g')
+        function handleMouseOver(e, d) {
+            console.log(d)
+            e.target.__data__.opacity = 1;
+
+            // // console.log(e)
+            // // console.log(e.target.__data__.title)
+            // d3.select(e.srcElement);
+
+            // d3.select('svg')
+            //     .append('text')
+            //     .attr('x', d.x)
+            //     .attr('y', d.y)
+            //     .attr('color', 'white')
+            //     .text(e.target.__data__.title)
+
+        }
+        const selection = d3.select(svgRef.current).select('g');
+        async function handleClick(id) {
+            setSelectedMovie(id);
+        }
+        const drag = d3.drag()
+            .on('drag', (e, d) => {
+                e.subject.fx = e.x;
+                e.subject.fy = e.y;
+                update();
+            })
+            .on('end', (e, d) => {
+                e.subject.fx = null;
+                e.subject.fy = null;
+            })
         function update() {
             if (!movieData)
                 return;
-            console.log('update')
-            //add the patterns of images
-            selection.call(zoom)
-            selection.append('defs')
-                .selectAll('pattern').data(movieData, d => d.id)
-                .join('pattern')
+
+            const circleGroups = selection.selectAll('g')
+                .data(movieData, d => d.id)
+                .enter()
+                .append('g')
+
+            selection.select('defs')
+                .selectAll('pattern')
+                .data(movieData, d => d.id)
+                .enter()
+                .append('pattern')
                 .attr('id', d => `image-${d.id}`)
                 .attr('width', 1)
                 .attr('height', 1)
+                .selectAll('image')
+                .data(movieData, d => d.id)
+                .enter()
                 .append('image')
                 .attr('href', d => d.image)
                 .attr('width', d => d.rad * 2)
                 .attr('height', d => d.rad * 2)
                 .attr('preserveAspectRatio', 'xMidYMid slice')
 
-            const circles = selection.selectAll('circle');
-            //on enter, bind them
-            selection.selectAll('circle').data(movieData, d => d.id)
-                .join('circle')
+            selection
+                .selectAll('circle')
+                .data(movieData, d => d.id)
                 .attr('cx', d => d.x)
                 .attr('cy', d => d.y)
-                .call(drag)
-                .attr("r", 0)
-                .style("opacity", 0)
-                .transition()
-                .duration(500)
-                .ease(d3.easeBackOut)  // Bouncy effect
-                .attr("r", d => d.rad)
-                .style("opacity", 1)
+                .on('click', (e, d) => handleClick(d.id))
+                .on('mouseover', handleMouseOver)
+                .enter()
+                .append('circle')
                 .attr('fill', d => `url(#image-${d.id})`)
+                .call(drag)
+                .attr('r', 0)
+                .transition()
+                .attr('r', d => d.rad)
 
-            // initDrag();
-            // initZoom();
+            selection
+                .selectAll('text')
+                .data(movieData, d => d.id)
+                .attr('x', d => d.x)
+                .attr('y', d => d.y - (d.rad + 5))
+                .attr('text-anchor', 'middle')
+                .enter()
+                .append('text')
+                .text(d => d.title)
+                .attr('font-size', '2em')
+                .attr('fill', 'white')
+                .attr('stroke', 'black')
+                .style('stroke-width', '1px')
+                .style('stroke-linejoin', 'round')
+                .attr('opacity', 1)
+                .raise()
+                .transition()
+                .delay(1000)
+                .duration(1000)
+                .attr('opacity', d => d.opacity)
+
+
         }
-        d3.forceSimulation(movieData)
+        const simulation = d3.forceSimulation(movieData)
             .force('x', d3.forceX(width / 2).strength(0.01))
             .force('y', d3.forceY(height / 2).strength(0.01))
             .force('charge', d3.forceManyBody().strength(10))
@@ -90,33 +159,25 @@ export default function SimilarGraph({ movieId }) {
             // .force('links', d3.forceLink(links).id(d => d.id).strength(0.01))
             .on('tick', update);
 
-        function handleDrag(e) {
-            e.subject.x = e.x;
-            e.subject.y = e.y;
-            update();
-        }
-        const drag = d3.drag().on('drag', handleDrag);
-        // function initDrag() {
-        //     d3.select('svg g').selectAll('circle').call(drag);
-        // }
+        update();
+        return () => simulation.stop();
+    }, [movieData.length])
+    useEffect(() => {
+        d3.select(svgRef.current).select('g').remove();
+        const svgd3 = d3.select(svgRef.current).attr('width', width).attr('height', height);
+        svgd3.append('g').append('defs')
         function handleZoom(e) {
             d3.select('g')
                 .attr('transform', e.transform);
         }
         const zoom = d3.zoom().on('zoom', handleZoom);
-        // function initZoom() {
-        //     d3.select('svg').call(zoom);
-        // }
-        update();
-        return () =>
-            d3.select('svg g').remove()
-
-    }, [movieData])
-
+        svgd3.call(zoom);
+        return () => d3.select('svg g').remove();
+    }, [])
 
 
     return (
-        <svg>
+        <svg width={width} height={height} ref={svgRef}>
         </svg>
     )
 }
